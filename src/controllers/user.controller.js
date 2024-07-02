b2fb2aef0b4dea07044d12398bd9a1eacad88d94
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-07-02 22:02:00 +0530

    fix message of already exist email

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async (req, res) => {
     const existedUser = await User.findOne({ email });
 
     if (existedUser) {
-        throw new ApiError(409, "User with email or username already exists");
+        throw new ApiError(409, "email already exists");
     }
 
     const user = await User.create({
